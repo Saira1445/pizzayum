c58b5afe10ca2d7cc9f54bb3e2e4beb830367781
Saira1445 <73920549+Saira1445@users.noreply.github.com>
2021-02-16 16:23:30 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+function getmenu(){
+
+var htmldata;
+
+htmldata="<ol class='menulist'>"
+menu_list_array.sort();
+for(var i=0<menu_list_array.leght; i++){
+    htmldata=-htmldat+'<li>' +menu_list_array[i] +
+'<li>'
+}
+
+htmldata=htmldata+ "<ol>"
+document.getElementById("display").innerHTML = htmldata;
+}
+
+
+function add_item(){
+    var htmldata;
+    var item=document.getElementById("add_item").nodeValue;
+    menu_list_array.push(item);
+    menu_list_array.sort();
+    htmldata="section class='cards>"
+    for(var i=0<menu_list_array.leght; i++){
+      htmldata=htmldata+ '<div class="card">' + "<img src="images/pizzaImg.png"/>" = menu_list_array[i] + '</div>' 
+    }
+
+    htmldata=htmldata+"</section>"
+document.getElementById("display_addmenu").innerHTML = htmldata;
+}
